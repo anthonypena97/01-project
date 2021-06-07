31e67a66e4d9cc121712d7c874b704804f42a7f8
Anthony Pena <apena5@alumni.jh.edu>
2021-06-06 19:56:19 -0600

    implemented getCurrentPosition

--- Web-Api-Demo/assets/js/script.js
+++ Web-Api-Demo/assets/js/script.js
@@ -1,13 +1,36 @@
-fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&limit=1000", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "8d516f5377msh83238b25d8f111fp1c55fejsnd6aa548c8ae2",
-		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-	}
-})
-	.then(function (response) {
-		return response.json();
+function myFunction() {
+	fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&limit=1000", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "8d516f5377msh83238b25d8f111fp1c55fejsnd6aa548c8ae2",
+			"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+		}
 	})
-	.then(function (data) {
-		console.log(data);
-	});
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+		});
+
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+
+	var x = document.getElementById("demo");
+
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	};
+	function showPosition(position) {
+		x.innerHTML = "Latitude: " + position.coords.latitude +
+			"<br>Longitude: " + position.coords.longitude;
+	};
+}
