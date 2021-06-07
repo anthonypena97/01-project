8bdb7a1799e82bee8b9ea248aa0a417959aeadab
Anthony Pena <apena5@alumni.jh.edu>
2021-06-07 16:20:12 -0600

    coordinates stored and applied to fetch

--- Web-Api-Demo/assets/js/script.js
+++ Web-Api-Demo/assets/js/script.js
@@ -1,24 +1,10 @@
 function myFunction() {
-	fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&limit=1000", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-key": "8d516f5377msh83238b25d8f111fp1c55fejsnd6aa548c8ae2",
-			"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-		}
-	})
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data);
-		});
-
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(showPosition);
 	}
 	else {
 		x.innerHTML = "Geolocation is not supported by this browser.";
-	}
+	};
 
 	var x = document.getElementById("demo");
 
@@ -29,8 +15,28 @@ function myFunction() {
 			x.innerHTML = "Geolocation is not supported by this browser.";
 		}
 	};
+
 	function showPosition(position) {
-		x.innerHTML = "Latitude: " + position.coords.latitude +
-			"<br>Longitude: " + position.coords.longitude;
+		var latData = position.coords.latitude;
+		var lonData = position.coords.longitude;
+		x.innerHTML = "Latitude: " + latData +
+			"<br>Longitude: " + lonData;
+		console.log(lonData);
+
+		fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&radius=25"
+			+ "&lon=" + lonData
+			+ "&lat=" + latData, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-key": "8d516f5377msh83238b25d8f111fp1c55fejsnd6aa548c8ae2",
+				"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+			}
+		})
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (data) {
+				console.log(data);
+			})
 	};
 }
