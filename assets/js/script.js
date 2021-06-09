34c17796e59d8b3278c6f5b1cdaf72bf1d9739e2
Anthony Pena <apena5@alumni.jh.edu>
2021-06-09 16:14:05 -0600

    function labeling + state display + removed extra function3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function locationSearch() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
@@ -41,6 +41,9 @@ function myFunction() {
                 var stateRaw = data.places[0].state;
                 var stateData = stateRaw.toLowerCase()
                 console.log(stateData);
+
+                var locationName = document.getElementById("locationName");
+                locationName.innerHTML = stateRaw
 
                 var randomSearch = Math.floor(Math.random() * 100)
                 console.log(randomSearch)
@@ -98,7 +101,11 @@ function myFunction() {
     };
 }
 
-function myFunction2() {
+function stateSearch() {
+    var x = document.getElementById("demo");
+    x.innerHTML = ""
+
+
     fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&q[state_cont]=washington", {
         "method": "GET",
         "headers": {
@@ -116,6 +123,9 @@ function myFunction2() {
             var stateRaw = data.places[0].state;
             var stateData = stateRaw.toLowerCase()
             console.log(stateData);
+
+            var locationName = document.getElementById("locationName");
+            locationName.innerHTML = stateRaw
 
             var randomSearch = Math.floor(Math.random() * 100)
             console.log(randomSearch)
@@ -172,81 +182,10 @@ function myFunction2() {
         })
 };
 
-function myFunction2() {
-    fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&q[state_cont]=washington", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "8d516f5377msh83238b25d8f111fp1c55fejsnd6aa548c8ae2",
-            "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-        }
-    })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            console.log(data.places[0].city + " - " + data.places[0].name);
+function citySearch() {
+    var x = document.getElementById("demo");
+    x.innerHTML = ""
 
-            var stateRaw = data.places[0].state;
-            var stateData = stateRaw.toLowerCase()
-            console.log(stateData);
-
-            var randomSearch = Math.floor(Math.random() * 100)
-            console.log(randomSearch)
-
-            var trailOne = data.places[0].city + " - " + data.places[0].name;
-            var responseContainerEl = document.querySelector('#result-one');
-            responseContainerEl.innerHTML = trailOne;
-
-            var trailTwo = data.places[1].city + " - " + data.places[1].name;
-            var responseContainerEl = document.querySelector('#result-two');
-            responseContainerEl.innerHTML = trailTwo;
-
-            var trailThree = data.places[2].city + " - " + data.places[2].name;
-            var responseContainerEl = document.querySelector('#result-three');
-            responseContainerEl.innerHTML = trailThree;
-
-            var trailFour = data.places[3].city + " - " + data.places[3].name;
-            var responseContainerEl = document.querySelector('#result-four');
-            responseContainerEl.innerHTML = trailFour;
-
-            var trailFive = data.places[4].city + " - " + data.places[4].name;
-            var responseContainerEl = document.querySelector('#result-five');
-            responseContainerEl.innerHTML = trailFive;
-
-            fetch(
-                "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=eba4d59c3d1445ea6dcd1a28e8cca79f" +
-                "&tags=" + stateData + "outdoors" + "&format=json&nojsoncallback=1"
-            )
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    console.log(data.photos.photo[randomSearch].id);
-
-                    var flickrServer = data.photos.photo[randomSearch].server;
-                    var flickrId = data.photos.photo[randomSearch].id;
-                    var flickrSecret = data.photos.photo[randomSearch].secret;
-
-                    // Flickr image URL https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
-                    var imageUrl = "https://live.staticflickr.com/" + flickrServer + "/" + flickrId + "_" + flickrSecret + "_" + "m" + ".jpg";
-                    console.log(imageUrl);
-
-                    var responseContainerEl = document.querySelector("#image");
-
-                    responseContainerEl.innerHTML = '';
-
-                    var flickrImg = document.createElement('img');
-                    flickrImg.setAttribute('src', imageUrl);
-
-                    responseContainerEl.appendChild(flickrImg);
-                });
-
-        })
-};
-
-function myFunction3() {
     fetch("https://trailapi-trailapi.p.rapidapi.com/?q-activities_activity_type_name_eq=hiking&q[city_cont]=los angeles", {
         "method": "GET",
         "headers": {
@@ -264,6 +203,9 @@ function myFunction3() {
             var stateRaw = data.places[0].state;
             var stateData = stateRaw.toLowerCase()
             console.log(stateData);
+
+            var locationName = document.getElementById("locationName");
+            locationName.innerHTML = stateRaw
 
             var randomSearch = Math.floor(Math.random() * 100)
             console.log(randomSearch)
